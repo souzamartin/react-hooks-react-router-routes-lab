0b2cdb5d33c0e610c53432fa596567139cd403b7
Martin <souzam@sonic.net>
2022-11-10 15:56:05 -0500

    commit tree 9df7b3c6ce57f3478f1285ecdec0d251466d8beb

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const renderedActors = actors.map(actor => {
+    return (
+      <div key={actor.name}>
+        <h3>{actor.name}</h3>
+        <ul>
+          {actor.movies.map(movie => {
+            return <li key={movie}>{movie}</li>
+          })}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {renderedActors}
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,27 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+  const renderedMovies = movies.map(movie => {
+    return (
+      <div key={movie.title}>
+        <h3>{movie.title}</h3>
+        <p>{movie.time} minutes</p>
+        <ul>
+          {movie.genres.map(genre => {
+            return <li key={genre}>{genre}</li>
+          })}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {renderedMovies}
+    </div>
+  )
 }
 
 export default Movies;
